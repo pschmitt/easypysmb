95fb384c86d4d1fa384624b12570f1f7287fc52a
Philipp Schmitt <philipp@schmitt.co>
2016-11-02 15:16:23 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='easypysmb',
-    version='1.4',
+    version='1.4.1',
     license='GPL3',
     description='Easy to use PySMB wrapper library',
     long_description=open('README.rst').read(),
