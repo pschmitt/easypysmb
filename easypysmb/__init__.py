269a1b148ea787e7db3e0e05180449439ea44053
Philipp Schmitt <philipp@schmitt.co>
2017-03-24 15:39:01 +0100

    Python 3 compatibility

--- easypysmb/__init__.py
+++ easypysmb/__init__.py
@@ -1,2 +1,3 @@
-from easypysmb import EasyPySMB
-from easypysmb import get_netbios_name
+from __future__ import absolute_import
+from .easypysmb import EasyPySMB
+from .easypysmb import get_netbios_name
