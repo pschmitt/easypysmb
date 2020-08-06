0206f98af7c8748840a3f61014814d706a5c03c1
Philipp Schmitt <philipp@schmitt.co>
2020-08-06 12:29:48 +0200

    Black.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='easypysmb',
-    version='1.4.3',
-    license='GPL3',
-    description='Easy to use PySMB wrapper library',
-    long_description=open('README.rst').read(),
-    author='Philipp Schmitt',
-    author_email='philipp@schmitt.co',
-    url='https://github.com/pschmitt/easypysmb',
+    name="easypysmb",
+    version="1.4.3",
+    license="GPL3",
+    description="Easy to use PySMB wrapper library",
+    long_description=open("README.rst").read(),
+    author="Philipp Schmitt",
+    author_email="philipp@schmitt.co",
+    url="https://github.com/pschmitt/easypysmb",
     packages=find_packages(),
-    install_requires=['pysmb'],
+    install_requires=["pysmb"],
 )
